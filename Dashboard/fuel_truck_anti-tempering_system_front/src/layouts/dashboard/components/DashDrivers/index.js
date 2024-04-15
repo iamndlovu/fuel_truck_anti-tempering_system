b0a56f724b8768f4a10b65dbf2ee4d5be8d73a01
Pardon Ndlovu <pndlovu385@icloud.com>
2024-04-15 19:48:02 +0200

    fixed drivers failing to load issue in new DashboardDrivers component

--- Dashboard/fuel_truck_anti-tempering_system_front/src/layouts/dashboard/components/DashDrivers/index.js
+++ Dashboard/fuel_truck_anti-tempering_system_front/src/layouts/dashboard/components/DashDrivers/index.js
@@ -1,18 +1,15 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import limitArray from "../../../../limitArray";
-import styles from "./index.module.css";
-import userAvater from "../../../../assets/images/user.png";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import limitArray from '../../../../limitArray';
+import styles from './index.module.css';
+import userAvater from '../../../../assets/images/user.png';
 
 const Drivers = () => {
-  const [allDrivers, setAllDrvers] = useState(null);
   const [limitedDrivers, setLimitedDrivers] = useState(null);
   useEffect(() => {
     axios
-      .get("http://localhost:1999/driver")
-      .then((res) => setAllDrvers(res.data.drivers))
-      .then(() => console.log(allDrivers))
-      .then(() => setLimitedDrivers(limitArray(allDrivers, 3)))
+      .get('http://localhost:1999/driver')
+      .then((res) => setLimitedDrivers(limitArray(res.data.drivers, 3)))
       .catch((err) =>
         console.error(`Failed to fetc drivers with err:\n${err}`)
       );
@@ -28,7 +25,7 @@ const Drivers = () => {
           limitedDrivers.map((driver) => (
             <li key={driver.id} className={styles.listItem}>
               <img src={userAvater} className={styles.avater} />
-              <div>
+              <div className={styles.driverDetails}>
                 <h6>Driver Name: {driver.name}</h6>
                 <h6>Driver ID: {driver.id}</h6>
                 <h6>Phone Number: {driver.phone}</h6>
